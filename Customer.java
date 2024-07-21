93d59f0d1db318427954adea05913024b367eb42
karthikolipe2412 <85942685+karthikolipe2412@users.noreply.github.com>
2024-07-21 12:48:39 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,10 @@
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    // Getters and Setters
+}
