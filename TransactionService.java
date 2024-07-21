a0d0cd3ef17bfbc3029f52d314ec088e4491df17
karthikolipe2412 <85942685+karthikolipe2412@users.noreply.github.com>
2024-07-21 12:53:57 +0530

    Create TransactionService.java

---
+++ TransactionService.java
@@ -0,0 +1,13 @@
+@Service
+public class TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getTransactionsByAccountId(Long accountId) {
+        return transactionRepository.findByAccountId(accountId);
+    }
+}
